115c8e66454ac46bf711ae88f38b722ecf46fca4
jidemobell <jidd32@gmail.com>
2018-09-08 18:01:48 +0100

    initialize app routes configuration

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require('./db/db');
 const routes = require('./routes/index');
 
+const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routes);
-app.listen(process.env.PORT || 4000);
+app.listen(port, () => {
+  console.log(`application started on port ${port}`);
+});
 
 module.exports = app;
