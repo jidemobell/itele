0455ef0cf2472eb3ae0993702d3b55811820132e
jidemobell <jidd32@gmail.com>
2018-09-08 16:32:55 +0100

    modify package dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   extends : "airbnb-base",
   env : {
     "mocha" : true,
+  },
+  rules: {
+    "linebreak-style": 0,
+    "consistent-return": 0,
+    "no-console": ["error", { allow: ["warn", "error"] }]
   }
 }
