e99350f995ca3a3f80ace0476b31581f66603828
jidemobell <jidd32@gmail.com>
2018-09-08 17:28:17 +0100

    update app server

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routes);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 
 module.exports = app;
