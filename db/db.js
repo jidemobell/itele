644f14e208fdc541078e17cf7cbaf3fb94fa851b
jidemobell <jidd32@gmail.com>
2018-09-10 16:54:21 +0100

    update for production

--- db/db.js
+++ db/db.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const path = require('path');
 const logger = require('tracer').colorConsole({ level: 'warn' });
 const getDbConfig = require('./config');
 
@@ -9,22 +7,5 @@ pool.on('error', (err, client) => { // eslint-disable-line no-unused-vars
   logger.error(err.stack);
 });
 
-fs.readFile(path.join(__dirname, 'init.sql'), 'utf-8', (err, data) => {
-  if (err) {
-    logger.error('Error reading sql file:', err.stack);
-  }
-
-  pool.connect()
-    .then((client) => {
-      return client.query(data)
-        .then(() => {
-          client.release();
-        });
-    })
-    .catch((error) => {
-      logger.error(error);
-      process.exit();
-    });
-});
 
 module.exports = pool;
