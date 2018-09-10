707bca57e50b405119bbe4b1506c4ca5cf8e0c7f
jidemobell <jidd32@gmail.com>
2018-09-10 01:09:14 +0100

    add GET /topactiveusers

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 
+
 const routes = express.Router();
+
+const { listUsersByActivity } = require('../db/models/users');
+
+routes.get('/topActiveUsers', (req, res) => {
+  const order = req.query.page;
+
+  listUsersByActivity(order).then((doc) => {
+    res.status(200).send(doc);
+  }).catch(err => res.status(400).send(err.stack));
+});
 
 module.exports = routes;
