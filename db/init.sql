562e226f73c4ac709ee76ce0934712fcfd7db85c
jidemobell <jidd32@gmail.com>
2018-09-10 14:24:58 +0100

    reorder sql query

--- db/init.sql
+++ db/init.sql
@@ -34,8 +34,8 @@ CREATE TABLE IF NOT EXISTS applications (
 );
 
 CREATE INDEX index_users_id ON users USING btree (id);
-CREATE index index_listings_id ON listings USING btree (id);
-CREATE index index_applications_id ON applications USING btree (id);
-CREATE index index_applications_user_id ON applications USING btree (user_id);
-CREATE index index_applications_listing_id ON applications USING btree (listing_id);
-CREATE index index_applications_created_at ON applications USING btree (created_at);
+CREATE INDEX index_listings_id ON listings USING btree (id);
+CREATE INDEX index_applications_id ON applications USING btree (id);
+CREATE INDEX index_applications_user_id ON applications USING btree (user_id);
+CREATE INDEX index_applications_listing_id ON applications USING btree (listing_id);
+CREATE INDEX index_applications_created_at ON applications USING btree (created_at);
