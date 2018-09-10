577917438c28211e60df6568f501a906af48735e
jidemobell <jidd32@gmail.com>
2018-09-11 00:25:39 +0100

    update GET /topActiveUsers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 
-
 const routes = express.Router();
 
-const { listUsersByActivity } = require('../db/models/users');
+const { listUsersByActivity } = require('../db/controller/users');
 
 routes.get('/topActiveUsers', (req, res) => {
   const order = req.query.page;
 
   listUsersByActivity(order).then((doc) => {
-    res.status(200).send(doc);
+    res.status(200).json(doc);
   }).catch(err => res.status(400).send(err.stack));
 });
 
