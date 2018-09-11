3307762bfe4701b7170958cc062c419593d39a7a
jidemobell <jidd32@gmail.com>
2018-09-11 13:14:20 +0100

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "linebreak-style": 0,
     "consistent-return": 0,
     "arrow-body-style": 0,
+    "prefer-destructuring":0,
     "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
   }
 }
