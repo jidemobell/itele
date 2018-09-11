a4604e99fa4b13b6bcf13d1f5e7fcae8d758c034
jidemobell <jidd32@gmail.com>
2018-09-11 13:02:31 +0100

    add GET /users

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const routes = express.Router();
 
-const { listUsersByActivity } = require('../db/controller/users');
+const { listUsersByActivity, getUser } = require('../db/controller/users');
 
 routes.get('/topActiveUsers', (req, res) => {
   const order = req.query.page;
 
   listUsersByActivity(order).then((doc) => {
+    res.status(200).json(doc);
+  }).catch(err => res.status(400).send(err.stack));
+});
+
+routes.get('/users', (req, res) => {
+  const id = req.query.id;
+
+  getUser(id).then((doc) => {
     res.status(200).json(doc);
   }).catch(err => res.status(400).send(err.stack));
 });
