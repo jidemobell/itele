db3656fda469609ad4c5beb00e50a297a0a8f4cd
jidemobell <jidd32@gmail.com>
2018-09-13 06:14:29 +0100

    update controller functions

--- db/controller/users.js
+++ db/controller/users.js
@@ -2,31 +2,34 @@ const { queryHelper } = require('../../libs/queryHelper');
 const { userInfo } = require('../const/info');
 const { topActiveUsers } = require('../const/active');
 
-module.exports = {
+/**
+  * select top active users
+  * @param  {number} val page
+  * @return {Object[]} array of user objects
+  */
+const getTopUsers = (val) => {
+  const query = {
+    text: topActiveUsers,
+    values: [val],
+  };
+  return queryHelper(query).then(response => response)
+    .catch((e) => { throw e; });
+};
 
-  /**
-   * select top active users
-   * @param  {number} val page
-   * @return {Object[]} array of user objects
-   */
-  listUsersByActivity(val) {
-    const query = {
-      text: topActiveUsers,
-      values: [val],
-    };
-    return queryHelper(query).then(response => response)
-      .catch((e) => { throw e; });
-  },
-  /**
-   * get user information
-   * @param  {number} id
-   */
-  getUser(id) {
-    const query = {
-      text: userInfo,
-      values: [id],
-    };
-    return queryHelper(query).then(response => response)
-      .catch((e) => { throw e; });
-  },
+/**
+  * get user information
+  * @param  {number} id
+  */
+const getUser = (id) => {
+  const query = {
+    text: userInfo,
+    values: [id],
+  };
+  return queryHelper(query).then(response => response)
+    .catch((e) => { throw e; });
+};
+
+module.exports = {
+  getTopUsers,
+  getUser,
 };
