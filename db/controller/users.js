31baf6a7ca5a018c457f718fb66fc67d5d8054e4
jidemobell <jidd32@gmail.com>
2018-09-13 18:42:23 +0100

    add  commentss to controller functions

--- db/controller/users.js
+++ db/controller/users.js
@@ -3,23 +3,32 @@ const { userInfo } = require('../const/info');
 const { topActiveUsers } = require('../const/active');
 
 /**
-  * select top active users
-  * @param  {number} val page
-  * @return {Object[]} array of user objects
-  */
-const getTopUsers = (val) => {
+ * list top active users for the week with latest applied listings.
+ * @function
+ * @name getTopUsers
+ * @memberof UserControllers
+ * @param {number} page - A page number.
+ * @returns {object[]} Returns an array of top users for the week.
+ */
+
+const getTopUsers = (page) => {
   const query = {
     text: topActiveUsers,
-    values: [val],
+    values: [page],
   };
   return queryHelper(query).then(response => response)
     .catch((e) => { throw e; });
 };
 
 /**
-  * get user information
-  * @param  {number} id
-  */
+ * list top active users for the week with latest applied listings.
+ * @function
+ * @name getUser
+ * @memberof UserControllers
+ * @param {number} id - A page number.
+ * @returns {object} Returns a user object.
+ */
+
 const getUser = (id) => {
   const query = {
     text: userInfo,
