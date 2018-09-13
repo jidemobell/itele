e745b52df5d1823776cb7bb63877e69a3e3b347e
jidemobell <jidd32@gmail.com>
2018-09-13 02:51:01 +0100

    add query input validation

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const { query, validationResult } = require('express-validator/check');
 
 const routes = express.Router();
 
 const { listUsersByActivity, getUser } = require('../db/controller/users');
 
-routes.get('/topActiveUsers', (req, res) => {
+
+const checkPageId = [
+  query('page').isInt(),
+];
+
+routes.get('/topActiveUsers', checkPageId, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   const order = req.query.page;
   listUsersByActivity(order).then((doc) => {
     res.status(200).json(doc);
   }).catch(err => res.status(400).send(err.stack));
 });
 
-routes.get('/users', (req, res) => {
+const checkUserId = [
+  query('id').isInt(),
+];
+
+routes.get('/users', checkUserId, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   const id = req.query.id;
   getUser(id).then((doc) => {
     res.status(200).json(doc);
