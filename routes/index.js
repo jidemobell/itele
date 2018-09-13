5d68de2e3926048ff14662dfacd95a9714a9a45c
jidemobell <jidd32@gmail.com>
2018-09-13 06:22:12 +0100

    update GET /topActiveUsers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { query, validationResult } = require('express-validator/check');
 
 const routes = express.Router();
 
-const { listUsersByActivity, getUser } = require('../db/controller/users');
+const { getTopUsers, getUser } = require('../db/controller/users');
 
 
 const checkPageId = [
@@ -16,7 +16,7 @@ routes.get('/topActiveUsers', checkPageId, (req, res) => {
     return res.status(422).json({ errors: errors.array() });
   }
   const order = req.query.page;
-  listUsersByActivity(order).then((doc) => {
+  getTopUsers(order).then((doc) => {
     res.status(200).json(doc);
   }).catch(err => res.status(400).send(err.stack));
 });
