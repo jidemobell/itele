7c9fe2401385289a9f110a67dc26ac5ee36f6815
jidemobell <jidd32@gmail.com>
2018-09-13 18:47:56 +0100

    add comment to db initializer function

--- db/initializer/loadScript.js
+++ db/initializer/loadScript.js
@@ -2,30 +2,34 @@ const fs = require('fs');
 const path = require('path');
 const logger = require('tracer').colorConsole();
 
-module.exports = {
-  /**
-   * This function loads the app with the
-   * latest database structure. existing
-   * are not repeated.
-   * @param  {} pool
-   */
-  loadDb(pool) {
-    fs.readFile(path.join(__dirname, 'db.sql'), 'utf-8', (err, data) => {
-      if (err) {
-        logger.error('Error reading init sql file:', err.stack);
-      }
 
-      pool.connect()
-        .then((client) => {
-          return client.query(data)
-            .then(() => {
-              client.release();
-            });
-        })
-        .catch((error) => {
-          logger.error(error);
-          process.exit();
-        });
-    });
-  },
+/**
+ * This function loads the app with the
+ * latest database structure. existing
+ * are not repeated.
+ * @function
+ * @name loadDb
+ * @param {} pool - A pg connection pool.
+ */
+
+const loadDb = (pool) => {
+  fs.readFile(path.join(__dirname, 'db.sql'), 'utf-8', (err, data) => {
+    if (err) {
+      logger.error('Error reading init sql file:', err.stack);
+    }
+
+    pool.connect()
+      .then((client) => {
+        return client.query(data)
+          .then(() => {
+            client.release();
+          });
+      })
+      .catch((error) => {
+        logger.error(error);
+        process.exit();
+      });
+  });
 };
+
+module.exports = { loadDb };
